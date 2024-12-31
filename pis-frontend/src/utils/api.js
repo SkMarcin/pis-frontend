f5a39602c7216654683d405fa3fd5ebd96fb95af
msikorsk <01178592@pw.edu.pl>
2024-12-31 08:03:18 +0100

    Modified gateway url as env variable

--- pis-frontend/src/utils/api.js
+++ pis-frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8180';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8180';
 
 export const signup = async (data) => {
     return await axios.post(`${API_BASE_URL}/auth/signup`, data);
