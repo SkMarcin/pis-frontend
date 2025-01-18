4fca0d98cc3e9a101dcfcb5eb7739fbbcd725564
msikorsk <01178592@pw.edu.pl>
2025-01-18 21:45:01 +0100

    Invalid path to file

--- pis-frontend/src/components/UsersPage.js
+++ pis-frontend/src/components/UsersPage.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import AuthContext from '../context/AuthContext';
-import './styles.css';
+import '../styles.css';
 
 const UsersPage = () => {
     const { user } = useContext(AuthContext);
