b4a48ccc3af694dfefd4e0f86f6a93d68bb3c517
Maciej Dobrowolski <maciejd644@gmail.com>
2025-01-26 06:50:58 +0100

    Added loans menu (without css, needs to be formatted)

--- pis-frontend/src/App.js
+++ pis-frontend/src/App.js
@@ -13,6 +13,7 @@ import UserLoans from "./components/loans/UserLoans";
 import AllLoans from "./components/loans/AllLoans";
 import BookLoans from "./components/loans/BookLoans";
 import ReturnBook from "./components/loans/ReturnBook";
+import Loans from "./components/loans/Loans"
 import './styles.css'
 import { Navigate } from 'react-router-dom';
 
@@ -28,6 +29,7 @@ const App = () => (
                 <Route path="/users" element={<UsersPage/>}/>
                 <Route path="/users/add" element={<AddUser />} />
                 <Route path="*" element={<NotFound />} />
+                <Route path="/loans" element={<Loans />} />
                 <Route path="/loans/add-loan" element={<AddLoan />} />
                 <Route path="/loans/user-loans/:userId" element={<UserLoans />} />
                 <Route path="/loans/all-loans" element={<AllLoans />} />
