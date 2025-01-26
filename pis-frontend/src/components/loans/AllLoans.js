7d96adef7efab96401b7255755a251185b217c2f
Maciej Dobrowolski <maciejd644@gmail.com>
2025-01-26 07:16:38 +0100

    Fixed displaying all loans. Loans are displayed in the table

--- pis-frontend/src/components/loans/AllLoans.js
+++ pis-frontend/src/components/loans/AllLoans.js
@@ -18,13 +18,30 @@ const AllLoans = () => {
     return (
         <div className="dashboard-container">
             <h1>All Loans</h1>
-            <ul>
-                {loans.map((loan) => (
-                    <li key={loan.id}>
-                        User ID: {loan.user_id}, Book ID: {loan.book_id}, From: {loan.date_from}, To: {loan.date_to}
-                    </li>
-                ))}
-            </ul>
+            <table className="min-w-full border border-collapse border-gray-300">
+                <thead>
+                    <tr className="bg-gray-200">
+                        <th className="border p-2">Loan ID</th>
+                        <th className="border p-2">User ID</th>
+                        <th className="border p-2">Book ID</th>
+                        <th className="border p-2">From</th>
+                        <th className="border p-2">To</th>
+                        <th className="border p-2">Returned</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {loans.map((loan) => (
+                        <tr key={loan.loanId} className="text-center">
+                            <td className="border p-2">{loan.loanId}</td>
+                            <td className="border p-2">{loan.user.userId}</td>
+                            <td className="border p-2">{loan.book.bookId}</td>
+                            <td className="border p-2">{loan.dateFrom}</td>
+                            <td className="border p-2">{loan.dateTo}</td>
+                            <td className="border p-2">{loan.returnDate ? loan.returnDate : 'Not returned'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
